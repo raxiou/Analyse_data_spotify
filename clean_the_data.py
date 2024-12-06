9b5912a822b0f6527e3419084b1a376d1c9c5acd
esdebroise <esteban.debroise@etudiant.univ-rennes1.fr>
2024-12-06 21:13:07 +0100

    start

---
+++ clean_the_data.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import plotly as py
+
+# Load the data
+data = pd.read_csv('data/spotify_songs.csv')
+
+# drop duplicates
+data = data.drop_duplicates(subset=['track_id', 'track_album_id', 'playlist_id'], inplace=True)
+
